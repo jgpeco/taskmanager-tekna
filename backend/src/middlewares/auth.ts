80916a71557982cf73b8780642469054ff96cc17
jgpeco <joaogabrielonofre@gmail.com>
2025-06-09 15:53:28 -0300

    fix: adjust return in auth middleware

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -2,14 +2,22 @@ import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 import { parseBearer } from '../utils/bearer'
 
-export function authenticateToken(req: Request, res: Response, next: NextFunction) {
+export function authenticateToken(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> | void {
   const authHeader = req.headers['authorization']
   const token = parseBearer(authHeader)
 
-  if (!token) return res.status(401).json({ message: 'Token not found.' })
+  if (!token) {
+    res.status(401).json({ message: 'Token not found.' })
+    return
+  }
 
   jwt.verify(token, process.env.JWT_SECRET as string, (err, user) => {
     if (err) return res.status(403).json({ message: 'Invalid token.' })
+    console.log('req user', user)
     req.user = user
     next()
   })
