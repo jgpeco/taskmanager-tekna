3495e4e6fcff650f27327833bb60130a0f98caf4
jgpeco <joaogabrielonofre@gmail.com>
2025-06-09 18:31:10 -0300

    feat: add task routes in index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes'
+import taskRoutes from './routes/task.routes'
 
 dotenv.config()
 const app = express()
 
 app.use(express.json())
 app.use('/auth', authRoutes)
+app.use('/api', taskRoutes)
 
 app.get('/health', (req: Request, res: Response) => {
   res.send('OK')
