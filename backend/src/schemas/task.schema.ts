c607db841c97ff601b54f168c239c1b65320184d
jgpeco <joaogabrielonofre@gmail.com>
2025-06-09 17:53:49 -0300

    feat: add new validations for task

--- backend/src/schemas/task.schema.ts
+++ backend/src/schemas/task.schema.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod'
 
 export const taskSchema = z.object({
-  title: z.string().min(1),
+  title: z
+    .string({ required_error: 'Title is required' })
+    .min(2, { message: 'Title must be at least 2 characters long' }),
   status: z.enum(['NEW', 'IN_PROGRESS', 'COMPLETED'])
 })
