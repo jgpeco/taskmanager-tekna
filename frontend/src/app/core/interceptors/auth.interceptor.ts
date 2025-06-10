d05c9927112713e859921bdb402daed1fd8af26b
jgpeco <joaogabrielonofre@gmail.com>
2025-06-10 18:57:29 -0300

    feat: create auth interceptor

---
+++ frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core'
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { AuthService } from '../../services/auth.service'
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  private readonly NO_AUTH_URLS = ['/auth/login', '/auth/register']
+
+  constructor(private authService: AuthService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const isAuthDisabledRoute = this.NO_AUTH_URLS.some((url) => request.url.startsWith(url))
+
+    if (isAuthDisabledRoute) {
+      return next.handle(request)
+    }
+
+    const authToken = this.authService.getToken()
+
+    if (authToken) {
+      const clonedRequest = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${authToken}`
+        }
+      })
+      return next.handle(clonedRequest)
+    }
+
+    return next.handle(request)
+  }
+}
