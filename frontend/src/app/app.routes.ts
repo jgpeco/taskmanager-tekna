68f950fe472962efde6ef809f1fd088f3f3368ac
jgpeco <joaogabrielonofre@gmail.com>
2025-06-10 15:28:04 -0300

    feat: create error page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router'
 import { LoginComponent } from './auth/login/login.component'
 import { RegisterComponent } from './auth/register/register.component'
+import { ErrorPageComponent } from './shared/error-page/error-page'
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'server-error', component: ErrorPageComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
 ]
---
+++ frontend/src/app/shared/error-page/error-page.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Router } from '@angular/router' // Para a funcionalidade de "voltar"
+
+// Angular Material Imports
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+
+@Component({
+  selector: 'app-error-page',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule],
+  templateUrl: './error-page.component.html',
+  styleUrls: ['./error-page.component.css']
+})
+export class ErrorPageComponent {
+  constructor(private router: Router) {}
+  goBack(): void {
+    history.back()
+  }
+}
