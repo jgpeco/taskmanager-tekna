41bb4fda2a2f3eb9765554a120c2cdf80a6e1c0a
jgpeco <joaogabrielonofre@gmail.com>
2025-06-10 14:37:05 -0300

    feat: initial setup of angular app

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterLink, RouterOutlet } from '@angular/router'
+
+// Angular Material Imports
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Task Manager'
+}
